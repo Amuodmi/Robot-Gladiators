6ae7490644539507d498880c02c0f593e6b6c107
Michelle <michelle.douma@gmail.com>
2022-05-12 22:25:10 -0700

    start of javascript

--- game.js
+++ game.js
@@ -1,1 +1,23 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+
+function fight(){
+      window.alert("The fight has begun!");
+    }
+
+var playerName = window.prompt("What is your robot's name?");
+
+// What is this?
+console.log(playerName);
+
+var playerName = "Tony the Robot";
+
+// Tony the Robot is ready for battle!
+console.log("Tony the Robot" + " is ready for battle!");
+
+// "Tony the Robot is ready for battle!"
+console.log(playerName + " is ready for battle!");
+
+// "Your robot, Tony the Robot, has won!
+console.log("Your robot, " + playerName + ", has won!");
+
+   // fight();
